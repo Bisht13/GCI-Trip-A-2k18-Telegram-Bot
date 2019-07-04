95454c69ddb093d84a73b80b25f6f93352b0e60b
Matej Plavevski <matej.plavevski+github@gmail.com>
2019-07-04 12:13:38 -0700

    Convert Strings to F strings

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 bot = telebot.TeleBot(token=os.environ['SUPERIOR_TELEGRAM_BOT_API'])
 
-instaIDs= open("instaIDs.txt","w+")
+instaIDs= open("instaIDs.txt", "w+")
 
 def find_at(msg):
     for text in msg:
@@ -20,21 +20,21 @@ def find_exclaimation(msg):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message,'Welcome!')
+    bot.reply_to(message, 'Welcome!')
 
 @bot.message_handler(commands=['Instagram'])
 def send_Instagram_Message(message):
-    bot.reply_to(message,'Enter username with id @')
+    bot.reply_to(message, 'Enter username with id @')
 
 @bot.message_handler(commands=['repo'])
 def send_Repo_URL(message):
-    bot.reply_to(message,'https://github.com/Bisht13/GCI-Trip-A-2k18-Telegram-Bot')
+    bot.reply_to(message, 'https://github.com/Bisht13/GCI-Trip-A-2k18-Telegram-Bot')
 
 @bot.message_handler(func= lambda msg: msg.text is not None and '@' in msg.text and 'id ' in msg.text)
 def send_Instgram_ID(message):
     texts = message.text.split()
     id = find_at(texts)
-    bot.reply_to(message, 'https://instagram.com/{}'.format(id[1:]))
+    bot.reply_to(message, f"https://instagram.com/{id[1:]}")
 
 @bot.message_handler(commands=['countdown'])
 def send_Countdown(message):
@@ -43,7 +43,7 @@ def send_Countdown(message):
     (d, t) = str(diff).split(',')
     (h, m, s) = str(t).split(':')
     s = int(round(float(s)))
-    bot.reply_to(message, d + ' ' + str(h) + " hours " + str(m) + " minutes " + str(s) + " seconds remaining")
+    bot.reply_to(message, f"{d} {str(h)} {hours} {str(m)} {minutes} {str(s)} seconds remaining")
 
 @bot.message_handler(commands=['ResetInstaIDs'])
 def reset_IDs():
@@ -52,7 +52,7 @@ def reset_IDs():
 
 @bot.message_handler(commands=['InstaAdd'])
 def reply_to_add_ID(message):
-    bot.reply_to(message,'To add your ID to /InstaList. Please write in the format given below \n !Yourname @YourID')
+    bot.reply_to(message, 'To add your ID to /InstaList. Please write in the format given below \n !Yourname @YourID')
 
 @bot.message_handler(func= lambda msg: msg.text is not None and '!' in msg.text and '@' in msg.text)
 def add_ID(message):
@@ -60,7 +60,7 @@ def add_ID(message):
     name = find_exclaimation(texts)[1:]
     id = find_at(texts)[1:]
     instaIDs = open('instaIDs.txt','a+')
-    instaIDs.write(name + '   @' + id + '\n')
+    instaIDs.write(f"{name}   @{id} \n")
     bot.reply_to(message, 'Your ID has been added :D')
 
 @bot.message_handler(commands=['InstaList'])
